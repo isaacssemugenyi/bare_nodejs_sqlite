6357826ba1a09b78e2a9f6cbd290efdded440c14
isaa ssemugenyi <isaacpro01@gmail.com>
2022-02-13 16:24:18 +0300

    Bare nodejs server

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const { getProducts, getProduct, createProduct, updateProduct, removeProduct } = require('./controllers/productController')
+
+
+const server = http.createServer((req, res)=>{
+    // res.statusCode = 200
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('<h1>Hello world!</h1>')
+    // res.end()
+
+    if(req.url === '/api/products' && req.method === 'GET'){
+        getProducts(req, res)
+    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+        const id = req.url.split('/')[3]
+        getProduct(req, res, id)
+    } else if(req.url === '/api/products' && req.method === 'POST'){
+        createProduct(req, res)
+    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT'){
+        const id = req.url.split('/')[3]
+        updateProduct(req, res, id)
+    } else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE'){
+        const id = req.url.split('/')[3]
+        removeProduct(req, res, id)
+    } else {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Route Not Found' }))  
+    }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ utils/utils.js
@@ -0,0 +1,31 @@
+const { writeFileSync } = require('fs')
+
+function writeDataToFile(filename, content){
+    writeFileSync(filename, JSON.stringify(content), 'utf8', (err) => {
+        if(err){
+            console.log(err)
+        }
+    })
+}
+
+function getPostData(req){
+    return new Promise((resolve, reject) => {
+        try {
+            let body = ''
+            req.on('data', (chunk) => {
+                body += chunk.toString()
+            })
+
+            req.on('end', () => {
+                resolve(body)
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+module.exports = {
+    writeDataToFile,
+    getPostData
+}
